5413e9490ca2ce22c99bbe5ebb2aeb841e723675
sajidgangat <sajidgangat.com>
2017-03-03 20:17:56 -0500

    firebase part implemented with the makority of work done

---
+++ assets/js/Firebase.js
@@ -0,0 +1,13 @@
+
+
+
+  // Initialize Firebase
+  var config = 
+  {
+    apiKey: "AIzaSyDHHA-KaQmEvElBRwqaZpwjvGl3m8FSS8Y",
+    authDomain: "locationdatabase-210d5.firebaseapp.com",
+    databaseURL: "https://locationdatabase-210d5.firebaseio.com",
+    storageBucket: "locationdatabase-210d5.appspot.com",
+    messagingSenderId: "342545487415"
+  };
+  firebase.initializeApp(config);
